8bcdeff0c8827a64381f27adef73e4877d615179
sou <ocelot2828@gmail.com>
2022-10-08 23:52:29 +0900

    new: all

---
+++ index.js
@@ -0,0 +1,20 @@
+module.exports = {
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      [
+        "new",
+        "ui",
+        "logic",
+        "add",
+        "remove",
+        "patch",
+        "update",
+        "return",
+        "bug",
+        "test",
+      ],
+    ],
+  },
+};
